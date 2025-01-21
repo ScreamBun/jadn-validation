9c70ef72ba420b3ad4d5e1176e79dfb20addefa3
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-01-21 14:10:08 -0500

    binary fixed up

--- jadnvalidation/tests/test_type_binary.py
+++ jadnvalidation/tests/test_type_binary.py
@@ -11,28 +11,26 @@ def test_binary():
     }
     
     bytes_1 = b"this is a test"
-    bytes_2 = "this is a test"
-    bytes_3 = bytearray([72, 101, 108, 108, 111])
-    bytes_invalid_1 = b'\xfc\xa1\xa1\xa1\xa1\xa1'
-    bytes_invalid_2 = b'\x80\x81\x82'
-    bytes_invalid_3 = bytearray("hello", "utf-16")
+    bytes_valid_2 = b'\x80\x81\x82'
+    bytes_invalid_2 = bytearray("hello", "utf-16")
     
     valid_data_1 = {'Binary-Test': bytes_1}
-    valid_data_2 = {'Binary-Test': bytes_2}
-    valid_data_3 = {'Binary-Test': bytes_3}
-    invalid_data_1 = {'Binary-Test': bytes_invalid_1}
+    valid_data_2 = {'Binary-Test': bytes_valid_2}
     invalid_data_2 = {'Binary-Test': bytes_invalid_2}
-    invalid_data_3 = {'Binary-Test': bytes_invalid_3}
     
     error_count = 0
-    pyd_model = create_pyd_model(jadn_schema)    
-    print(pyd_model)
+    try:
+        pyd_model = create_pyd_model(jadn_schema)    
+        print(pyd_model)
+    except Exception as e:
+        error_count = error_count + 1
+        print(e)
         
     try:
         pyd_model.model_validate(valid_data_1)
     except ValidationError as e:
         error_count = error_count + 1
-        print(e)              
+        print(e)                                         
         
     try:
         pyd_model.model_validate(valid_data_2)
@@ -40,19 +38,7 @@ def test_binary():
         error_count = error_count + 1
         print(e)
         
-    try:
-        pyd_model.model_validate(valid_data_3)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)                            
-        
-    assert error_count == 0        
-        
-    try:
-        pyd_model.model_validate(invalid_data_1)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)
+    assert error_count == 0         
         
     try:
         pyd_model.model_validate(invalid_data_2)
@@ -60,13 +46,7 @@ def test_binary():
         error_count = error_count + 1
         print(e)
         
-    try:
-        pyd_model.model_validate(invalid_data_3)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)                
-        
-    assert error_count == 3
+    assert error_count == 1
     
 def test_binary_min():
   
@@ -77,22 +57,24 @@ def test_binary_min():
     }
     
     bytes_1 = b"this is a test"
-    bytes_2 = "this is a test"
-    bytes_3 = bytearray([72, 101, 108, 108, 111])
-    bytes_invalid_1 = b''
-    bytes_invalid_2 = b'\x80'
-    bytes_invalid_3 = bytearray("z", "utf-16")
+    bytes_valid_2 = b'\x80\x81\x82'
+    bytes_invalid_1 = b"x"
+    bytes_invalid_2 = bytearray("hello", "utf-16")
+    bytes_invalid_3 = b'\x80'
     
     valid_data_1 = {'Binary-Test': bytes_1}
-    valid_data_2 = {'Binary-Test': bytes_2}
-    valid_data_3 = {'Binary-Test': bytes_3}
+    valid_data_2 = {'Binary-Test': bytes_valid_2}
     invalid_data_1 = {'Binary-Test': bytes_invalid_1}
     invalid_data_2 = {'Binary-Test': bytes_invalid_2}
     invalid_data_3 = {'Binary-Test': bytes_invalid_3}
     
     error_count = 0
-    pyd_model = create_pyd_model(jadn_schema)    
-    print(pyd_model)
+    try:
+        pyd_model = create_pyd_model(jadn_schema)    
+        print(pyd_model)
+    except Exception as e:
+        error_count = error_count + 1
+        print(e)
         
     try:
         pyd_model.model_validate(valid_data_1)
@@ -104,13 +86,7 @@ def test_binary_min():
         pyd_model.model_validate(valid_data_2)
     except ValidationError as e:
         error_count = error_count + 1
-        print(e)
-        
-    try:
-        pyd_model.model_validate(valid_data_3)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)                            
+        print(e)                      
         
     assert error_count == 0        
         
@@ -142,23 +118,25 @@ def test_binary_max():
       ]
     }
     
-    bytes_1 = b'test'
-    bytes_2 = 'test'
-    bytes_3 = bytearray([72, 101, 108, 108])
-    bytes_invalid_1 = b"this is a test"
-    bytes_invalid_2 = "this is a test"
-    bytes_invalid_3 = bytearray([72, 101, 108, 108, 111])    
+    bytes_1 = b"test"
+    bytes_valid_2 = b'\x80\x81\x82'
+    bytes_invalid_1 = b"zzzzzz"
+    bytes_invalid_2 = bytearray("hello", "utf-16")
+    bytes_invalid_3 = b'\x80\x80\x80\x80\x80\x80'
     
     valid_data_1 = {'Binary-Test': bytes_1}
-    valid_data_2 = {'Binary-Test': bytes_2}
-    valid_data_3 = {'Binary-Test': bytes_3}
+    valid_data_2 = {'Binary-Test': bytes_valid_2}
     invalid_data_1 = {'Binary-Test': bytes_invalid_1}
     invalid_data_2 = {'Binary-Test': bytes_invalid_2}
     invalid_data_3 = {'Binary-Test': bytes_invalid_3}
     
     error_count = 0
-    pyd_model = create_pyd_model(jadn_schema)    
-    print(pyd_model)
+    try:
+        pyd_model = create_pyd_model(jadn_schema)    
+        print(pyd_model)
+    except Exception as e:
+        error_count = error_count + 1
+        print(e)
         
     try:
         pyd_model.model_validate(valid_data_1)
@@ -170,13 +148,7 @@ def test_binary_max():
         pyd_model.model_validate(valid_data_2)
     except ValidationError as e:
         error_count = error_count + 1
-        print(e)
-        
-    try:
-        pyd_model.model_validate(valid_data_3)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)                            
+        print(e)                      
         
     assert error_count == 0        
         
@@ -204,27 +176,29 @@ def test_binary_min_max():
   
     jadn_schema = {
       "types": [
-        ["Binary-Test", "Binary", ["{6","}8"], ""]
+        ["Binary-Test", "Binary", ["{4","}8"], ""]
       ]
     }
     
-    bytes_1 = b'testing'
-    bytes_2 = 'testing'
-    bytes_3 = bytearray([72, 101, 108, 108, 108, 108])
-    bytes_invalid_1 = b"this is a test"
-    bytes_invalid_2 = "test"
-    bytes_invalid_3 = bytearray([72, 101, 108, 108, 111])    
+    bytes_1 = b"test"
+    bytes_valid_2 = b'\x80\x81\x82\x82'
+    bytes_invalid_1 = b"zzzzzzzzzzz"
+    bytes_invalid_2 = bytearray("hello", "utf-16")
+    bytes_invalid_3 = b'\x80\x80\x80\x80\x80\x80\x80\x80\x80'
     
     valid_data_1 = {'Binary-Test': bytes_1}
-    valid_data_2 = {'Binary-Test': bytes_2}
-    valid_data_3 = {'Binary-Test': bytes_3}
+    valid_data_2 = {'Binary-Test': bytes_valid_2}
     invalid_data_1 = {'Binary-Test': bytes_invalid_1}
     invalid_data_2 = {'Binary-Test': bytes_invalid_2}
     invalid_data_3 = {'Binary-Test': bytes_invalid_3}
     
     error_count = 0
-    pyd_model = create_pyd_model(jadn_schema)    
-    print(pyd_model)
+    try:
+        pyd_model = create_pyd_model(jadn_schema)    
+        print(pyd_model)
+    except Exception as e:
+        error_count = error_count + 1
+        print(e)
         
     try:
         pyd_model.model_validate(valid_data_1)
@@ -236,13 +210,7 @@ def test_binary_min_max():
         pyd_model.model_validate(valid_data_2)
     except ValidationError as e:
         error_count = error_count + 1
-        print(e)
-        
-    try:
-        pyd_model.model_validate(valid_data_3)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)                            
+        print(e)                      
         
     assert error_count == 0        
         
