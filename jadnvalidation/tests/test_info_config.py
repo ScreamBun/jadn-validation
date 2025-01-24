41253e6775fbd1dbec339e95ac6a931ac3af84f8
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-01-24 08:52:30 -0500

    max string order of precedence test

--- jadnvalidation/tests/test_info_config.py
+++ jadnvalidation/tests/test_info_config.py
@@ -51,3 +51,53 @@ def test_max_string():
         print(e)         
         
     assert error_count == 1
+    
+def test_max_string_order_of_precedence():
+  
+    jadn_schema = {
+        "info": {
+            "package": "http://test/v1.0",
+            "title": "Test Title",
+            "exports": ["String-Name"],
+            "config": {
+            "$MaxBinary": 255,
+            "$MaxString": 255,
+            "$MaxElements": 100,
+            "$Sys": "$",
+            "$TypeName": "^[A-Z][-$A-Za-z0-9]{0,63}$",
+            "$FieldName": "^[a-z][_A-Za-z0-9]{0,63}$",
+            "$NSID": "^[A-Za-z][A-Za-z0-9]{0,7}$"
+            }
+        },
+        "types": [
+            ["String-Name", "String", ["}5"], ""]
+        ]
+    }
+    
+    valid_data_1 = {'String-Name': 'asdf'}
+    invalid_data_1 = {'String-Name': 'asdfghjklasdfghjkl'}
+    
+    error_count = 0
+    pyd_model = {}
+    
+    try :
+        pyd_model = create_pyd_model(jadn_schema)
+    except Exception as err:
+        err_count = err_count + 1
+        print(err)        
+        
+    try :
+        data_validation(pyd_model, valid_data_1)
+    except Exception as err:
+        err_count = err_count + 1
+        print(err)           
+        
+    assert error_count == 0        
+        
+    try:
+        data_validation(pyd_model, invalid_data_1)
+    except Exception as e:
+        error_count = error_count + 1
+        print(e)         
+        
+    assert error_count == 1    
