d65216955384692d55b9d3e1f26ab1ec012c5210
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-02-26 10:41:35 -0500

    grabbing arrays logic set

--- jadnvalidation/jadn_custom_validation.py
+++ jadnvalidation/jadn_custom_validation.py
@@ -1,10 +1,27 @@
-def custom_validation(jadn_schema: dict, data: dict):
+from jadnvalidation.models.jadn.jadn_type import Base_Type, build_jadn_type_obj
+
+
+def get_types(j_types: list, base_type: str):
+    return [j_type for j_type in j_types if j_type[1] == base_type]
+
+def arrary_validation(j_schema: dict, data: dict):
+    """
+    Custom validation for jadn arrays. 
+    """
+    j_types = j_schema.get('types')
+    arrays = get_types(j_types, Base_Type.ARRAY.value)
+    for array in arrays:
+        j_type_obj = build_jadn_type_obj(array, {})
+        test = ""
+    
+def custom_validation(j_schema: dict, data: dict):
     """
     Custom validations needed to cover pydantic limitations. 
     For instance, jadn array validation.  
     """
     
     # TODO: check if schema contains and array and if data contains the array name. 
+    arrary_validation(j_schema, data)
     test = ""
     
     
---
+++ jadnvalidation/tests/test_custom_validation.py
@@ -0,0 +1,27 @@
+from jadnvalidation.jadn_custom_validation import arrary_validation
+
+
+def test_arrary_validation():
+    j_schema = {
+        "types": [
+            ["Array-Test", "Array", [], "", [
+                [1, "field_value_1", "String", [], ""],
+                [2, "field_value_2", "Boolean", [], ""],
+                [3, "field_value_3", "Integer", [], ""]
+            ]]
+        ]
+    }
+    
+    valid_data_list = [
+            { "Array-Test": ["test", True, 123] },
+            { "Array-Test": [123, "test", True] },
+            { "Array-Test": ["test", "test", "test"] },
+            { "Array-Test": ["test", "test", "test", 123, "test", "test", False] }
+        ]    
+    
+    try :
+        arrary_validation(j_schema, valid_data_list[0])
+    except Exception as err:
+        print(err)       
+    
+    assert True    
