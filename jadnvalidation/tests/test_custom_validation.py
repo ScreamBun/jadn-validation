341c821902da9864ff3bbd16878dc7fa87ae38dd
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-02-27 13:11:47 -0500

    3 invalid tests added

--- jadnvalidation/tests/test_custom_validation.py
+++ jadnvalidation/tests/test_custom_validation.py
@@ -4,7 +4,7 @@ from jadnvalidation.jadn_custom_validation import custom_validation
 def test_arrary_validation():
     j_schema = {
         "types": [
-            ["Array-Test", "Array", ["{10"], "", [
+            ["Array-Test", "Array", ["{3", "}3"], "", [
                 [1, "field_value_1", "String", [], ""],
                 [2, "field_value_2", "Boolean", [], ""],
                 [3, "field_value_3", "Integer", [], ""]
@@ -12,17 +12,29 @@ def test_arrary_validation():
         ]
     }
     
-    data_list = [
+    valid_data_list = [
             { "Array-Test": ["test", True, 123] },
+        ]  
+    
+    invalid_data_list = [
+            { "Array-Test": ["test", True] },
             { "Array-Test": "test" },
-            { "Array-Test": [123, "test", True] },
-            { "Array-Test": ["test", "test", "test"] },
             { "Array-Test": ["test", "test", "test", 123, "test", "test", False] }
-        ]    
+        ]        
     
     try :
-        custom_validation(j_schema, data_list[0])
+        for data in valid_data_list:
+            custom_validation(j_schema, data)
     except Exception as err:
-        print(err)       
+        print(err)
+        assert False     
+                
+    err_count = 0                
+    for data in invalid_data_list:
+        try :
+            custom_validation(j_schema, data)
+        except Exception as err:
+            print(err)
+            err_count += 1
     
-    assert True    
+    assert len(invalid_data_list) == err_count
