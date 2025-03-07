ff1a3ec539f7e4ac3d9e2a9ad824e4f0fd1890b1
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-03-07 11:05:20 -0500

    int unit tests updated formats are failing as expected

--- jadnvalidation/tests/test_int.py
+++ jadnvalidation/tests/test_int.py
@@ -76,196 +76,56 @@ def test_type_int_max():
     err_count = validate_valid_data(j_schema, root, invalid_data_list)
     assert err_count == len(invalid_data_list)
 
-# TODO: Leftoff here
 def test_type_int_i8():
+    root = "Root-Test"
   
-    jadn_schema = {
+    j_schema = {
       "types": [
-        ["Integer-Instance", "Integer", ["/i8"], ""]
+        ["Root-Test", "Integer", ["/i8"], ""]
       ]
     }
-      
-    i8_instance_data_1 = {'Integer-Instance': 1}
-    i8_instance_data_2 = {'Integer-Instance': 0}
-    i8_instance_data_invalid_1 = {'Integer-Instance': 5555}
-    i8_instance_data_invalid_2 = {'Integer-Instance': -3333}  
-    i8_instance_data_invalid_3 = {'Integer-Instance': "twelve-teen"}  
-  
-    error_count = 0
-    pyd_model = create_pyd_model(jadn_schema)    
-    print(pyd_model)  
-
-    try:
-        pyd_model.model_validate(i8_instance_data_1)
-        pyd_model.model_validate(i8_instance_data_2)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)
-        
-    assert error_count == 0  
     
-    try:
-        pyd_model.model_validate(i8_instance_data_invalid_1)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)
+    valid_data_list = [0, 1]      
+    invalid_data_list = [5555, -3333, "thirteen-teen"] 
+  
+    err_count = validate_valid_data(j_schema, root, valid_data_list)    
+    assert err_count == 0
         
-    try:
-        pyd_model.model_validate(i8_instance_data_invalid_2)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)  
-    try:
-        pyd_model.model_validate(i8_instance_data_invalid_3)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)  
-
-    assert error_count == 3
+    err_count = validate_valid_data(j_schema, root, invalid_data_list)
+    assert err_count == len(invalid_data_list)
     
 def test_type_int_i16():
+    root = "Root-Test"
   
-    jadn_schema = {
+    j_schema = {
       "types": [
-        ["Integer-Instance", "Integer", ["/i16"], ""]
+        ["Root-Test", "Integer", ["/i16"], ""]
       ]
     }
-      
-    i16_instance_data_1 = {'Integer-Instance': 1}
-    i16_instance_data_2 = {'Integer-Instance': 0}
-    i16_instance_data_invalid_1 = {'Integer-Instance': 555555}
-    i16_instance_data_invalid_2 = {'Integer-Instance': -333333}  
-    i16_instance_data_invalid_3 = {'Integer-Instance': "thirteen-teen"}  
-  
-    error_count = 0
-    pyd_model = create_pyd_model(jadn_schema)    
-    print(pyd_model)
-
-    try:
-        pyd_model.model_validate(i16_instance_data_1)
-        pyd_model.model_validate(i16_instance_data_2)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)
-        
-    assert error_count == 0  
     
-    try:
-        pyd_model.model_validate(i16_instance_data_invalid_1)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)
+    valid_data_list = [0, 1]      
+    invalid_data_list = [555555, -333333, "four-teen"] 
+  
+    err_count = validate_valid_data(j_schema, root, valid_data_list)    
+    assert err_count == 0
         
-    try:
-        pyd_model.model_validate(i16_instance_data_invalid_2)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)  
-    try:
-        pyd_model.model_validate(i16_instance_data_invalid_3)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)  
-
-    assert error_count == 3
+    err_count = validate_valid_data(j_schema, root, invalid_data_list)
+    assert err_count == len(invalid_data_list)
     
 def test_type_int_i32():
+    root = "Root-Test"    
   
-    jadn_schema = {
+    j_schema = {
       "types": [
-        ["Integer-Instance", "Integer", ["/i32"], ""]
+        ["Root-Test", "Integer", ["/i32"], ""]
       ]
     }
-      
-    i32_instance_data_1 = {'Integer-Instance': 1}
-    i32_instance_data_2 = {'Integer-Instance': 0}
-    i32_instance_data_invalid_1 = {'Integer-Instance': 5555555555}
-    i32_instance_data_invalid_2 = {'Integer-Instance': -3333333333}  
-    i32_instance_data_invalid_3 = {'Integer-Instance': "a dozen dozen, aka a gross"}  
-  
-    error_count = 0
-    pyd_model = create_pyd_model(jadn_schema)    
-    print(pyd_model)
-
-    try:
-        pyd_model.model_validate(i32_instance_data_1)
-        pyd_model.model_validate(i32_instance_data_2)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)
-        
-    assert error_count == 0  
     
-    try:
-        pyd_model.model_validate(i32_instance_data_invalid_1)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)
-        
-    try:
-        pyd_model.model_validate(i32_instance_data_invalid_2)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)  
-    try:
-        pyd_model.model_validate(i32_instance_data_invalid_3)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)  
-
-    assert error_count == 3
-
-def test_type_int_uN():
+    valid_data_list = [0, 1]      
+    invalid_data_list = [5555555555, -3333333333, "dozen dozen"] 
   
-    jadn_schema = {
-      "types": [
-        ["Integer-Instance", "Integer", ["/u2"], ""]
-      ]
-    }
-      
-    uN_instance_data_1 = {'Integer-Instance': 1}
-    uN_instance_data_2 = {'Integer-Instance': 0}
-    uN_instance_data_3 = {'Integer-Instance': 3}
-    uN_instance_data_invalid_1 = {'Integer-Instance': 5}
-    uN_instance_data_invalid_2 = {'Integer-Instance': -1}  
-    uN_instance_data_invalid_3 = {'Integer-Instance': "pi"}  
-  
-    error_count = 0
-    pyd_model = create_pyd_model(jadn_schema)    
-    print(pyd_model)
-
-    try:
-        pyd_model.model_validate(uN_instance_data_1)
-        pyd_model.model_validate(uN_instance_data_2)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)
+    err_count = validate_valid_data(j_schema, root, valid_data_list)    
+    assert err_count == 0
         
-    assert error_count == 0  
-
-    try:
-        pyd_model.model_validate(uN_instance_data_3)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)
-        
-    assert error_count == 0  
-    
-    try:
-        pyd_model.model_validate(uN_instance_data_invalid_1)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)
-        
-    try:
-        pyd_model.model_validate(uN_instance_data_invalid_2)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)  
-    try:
-        pyd_model.model_validate(uN_instance_data_invalid_3)
-    except ValidationError as e:
-        error_count = error_count + 1
-        print(e)  
-
-    assert error_count == 3
+    err_count = validate_valid_data(j_schema, root, invalid_data_list)
+    assert err_count == len(invalid_data_list)
