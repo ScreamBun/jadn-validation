a625fb8a5569627d86b36fd2fb9119f8ccdd4d60
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-03-13 09:47:18 -0400

    arrayof booleans ready

--- jadnvalidation/tests/test_array_of.py
+++ jadnvalidation/tests/test_array_of.py
@@ -87,4 +87,33 @@ def test_array_of_strs():
     assert err_count == 0
         
     err_count = validate_invalid_data(j_schema, root, invalid_data_list)    
-    assert err_count == 2    
+    assert err_count == 2
+    
+def test_array_of_booleans():
+    root = "Root-Test"    
+    
+    j_schema = {
+        "info": {
+            "package": "http://www.test.com",
+            "exports": ["Root-Test"]
+        },
+        "types": [
+            ["Root-Test", "ArrayOf", ["*Boolean", "{1", "}3"], ""]
+        ]
+    }
+    
+    valid_data_list = [
+            [True, False],
+            [False, False, True]
+        ]
+    
+    invalid_data_list = [
+            ["True", "False", 1],
+            ["true", True, False],
+        ]
+        
+    err_count = validate_valid_data(j_schema, root, valid_data_list)    
+    assert err_count == 0
+        
+    err_count = validate_invalid_data(j_schema, root, invalid_data_list)    
+    assert err_count == 2      
