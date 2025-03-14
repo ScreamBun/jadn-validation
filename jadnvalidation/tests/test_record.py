fbff110667141a1a74d1a9cbf77456c330bef1d5
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-03-14 13:54:27 -0400

    all record tests pass

--- jadnvalidation/tests/test_record.py
+++ jadnvalidation/tests/test_record.py
@@ -3,116 +3,61 @@ from jadnvalidation.utils.general_utils import create_clz_instance
 
 
 def test_forward_ref():
+    root = "Root-Test"
     
     j_schema =   {  
         "types": [
-            ["RecordName1", "Record", [], "", [
-                [1, "field_value_1a", "RecordName2", [], ""]
+            ["Root-Test", "Record", [], "", [
+                [1, "field_value_a", "RecordName2", [], ""]
             ]],
             ["RecordName2", "Record", [], "", [
-                [1, "field_value_2a", "String", [], ""]
-            ]]
-        ]
-    }
-    
-    valid_data_list = [{
-        'RecordName1': {
-            'field_value_1a': {
-                'field_value_2a': 'Anytown'
-            }  
-        },
-        'RecordName2': {
-            'field_value_2a': 'Anytown'
-        }             
-    }]
-    
-    invalid_data_list = [{
-        'RecordName1': {
-            'field_value_1a': {
-                'field_value_2a': 'Anytown'
-            }  
-        },
-        'RecordName2': {
-            'field_value_2zzzz': False
-        }
-    },
-    { 'true' : 'True' },
-    {}]                         
-    
-    custom_schema, err_count = create_testing_model(j_schema)
-        
-    err_count = validate_valid_data(custom_schema, valid_data_list)    
-    assert err_count == 0
-        
-    err_count = validate_invalid_data(custom_schema, invalid_data_list)
-    assert err_count == len(invalid_data_list) 
-
-def test_forward_ref_deeper():
-    
-    j_schema =   {  
-        "types": [
-            ["RecordName1", "Record", [], "", [
-                [1, "field_value_1a", "RecordName2", [], ""]
-            ]],
-            ["RecordName2", "Record", [], "", [
-                [1, "field_value_2a", "RecordName3", [], ""]
+                [1, "field_value_aa", "RecordName3", [], ""]
             ]],
             ["RecordName3", "Record", [], "", [
-                [1, "field_value_3a", "RecordName4", [], ""]
+                [1, "field_value_aaa", "RecordName4", [], ""]
             ]],
             ["RecordName4", "Record", [], "", [
-                [1, "field_value_4a", "String", [], ""]
+                [1, "field_value_aaaa", "String", [], ""]
             ]]
         ]
     }
     
-    valid_data_list = [{
-        'RecordName1': {
-            'field_value_1a': {
-                'field_value_2a': {
-                    'field_value_3a': {
-                        'field_value_4a': 'Anytown'
+    valid_data_list = [
+        {
+            'field_value_a': {
+                'field_value_aa': {
+                    'field_value_aaa': {
+                        'field_value_aaaa': 'Darth Malgus'
+                    }
+                }
+            }  
+        }
+    ]
+    
+    invalid_data_list = [
+        {
+            'field_value_a': {
+                'field_value_aa': {
+                    'field_value_aaa': {
+                        'field_value_aaaa': 0
                     }
                 }
             }  
         },
-        'RecordName2': {
-            'field_value_2a': {
-                'field_value_3a': {
-                    'field_value_4a': 'Anytown'
-                }
-            }
+        {
+            'field_value_a': {
+                'field_value_aaa': "Dartg Nihilus"
+            },
         },
-        'RecordName3': {
-            'field_value_3a': {
-                'field_value_4a': 'Anytown'
-            }
-        },
-        'RecordName4': {
-            'field_value_4a': 'Anytown'
-        }               
-    }]
+        { 'true' : 'True' },
+        {}
+    ]
     
-    invalid_data_list = [{
-        'RecordName1': {
-            'field_value_1a': {
-                'field_value_2a': 'Anytown'
-            }  
-        },
-        'RecordName2': {
-            'field_value_2zzzz': False
-        }             
-    },
-    { 'true' : 'True' },
-    {}]
-    
-    custom_schema, err_count = create_testing_model(j_schema)
-        
-    err_count = validate_valid_data(custom_schema, valid_data_list)    
+    err_count = validate_valid_data(j_schema, root, valid_data_list)    
     assert err_count == 0
-          
-    err_count = validate_invalid_data(custom_schema, invalid_data_list)
-    assert err_count == len(invalid_data_list)  
+            
+    err_count = validate_valid_data(j_schema, root, invalid_data_list)
+    assert err_count == len(invalid_data_list) 
 
 def test_records_min_max():
     root = "Root-Test"
