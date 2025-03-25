e6673efd659058c227d7216c182e07d266f3ac7f
Kevin Cressman <kgcressman@praxiseng.com>
2025-03-25 16:01:30 -0400

    initializing branch w/ format changes

--- jadnvalidation/data_validation/binary.py
+++ jadnvalidation/data_validation/binary.py
@@ -1,6 +1,7 @@
 from typing import Union
 from jadnvalidation.models.jadn.jadn_type import Jadn_Type, build_j_type
-from jadnvalidation.utils.mapping_utils import get_max_length, get_min_length
+from jadnvalidation.utils.mapping_utils import get_max_length, get_min_length, get_opts
+from jadnvalidation.utils.general_utils import split_on_first_char
 
 rules = {
     "type": "check_type",
@@ -40,8 +41,26 @@ class Binary:
             self.errors.append(f"Binary length must be less than or equal to {max_length}. Received: {len(self.data)}")
         
     def check_format(self):
-        # TODO: binary formats...
-        tbd = ""
+
+        val = None
+        opts = get_opts(self.j_type)
+        for opt in opts:
+            opt_key, opt_val = split_on_first_char(opt)
+            if "/" == opt_key:
+
+                val = opt_val
+                format_min = None
+                format_max = None
+                if opt_val == 'eui':
+                    pass
+                elif opt_val == 'ipv4-addr':
+                    pass
+                elif opt_val == 'ipv6-addr':
+                    pass
+                if self.data > format_max:
+                    self.errors.append(f"Data exceeds allowed format length: {format_max}")
+                if self.data < format_min:
+                    self.errors.append(f"Data does not meet minimum format length: {format_min}")
         
     def validate(self):
         
