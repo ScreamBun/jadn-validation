2c1fd9abf36d885d9ec9d127d2eb25460fd105dd
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-04-04 14:03:33 -0400

    whl setup

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="jadnvalidation",
     version="1.0.0", 
-    packages=["jadnvalidation"],
-    install_requires=[]    
+    packages=["jadnvalidation", "jadnvalidation.data_validation", "jadnvalidation.data_validation.formats", "jadnvalidation.models", "jadnvalidation.models.jadn", "jadnvalidation.utils", "jadnvalidation.models.jadn"],
+    install_requires=[
+        "jsonpointer",
+        "pytest",
+        "netaddr",
+        "rfc3986",
+        "rfc3987",
+        "strict-rfc3339",
+        "typing-extensions",
+        "uritemplate",
+        "validators"
+    ]    
 )
