072997d4f57343855318ed3ad98cfaf7406ce978
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-04-14 09:55:27 -0400

    map xml test added

--- jadnvalidation/tests/test_map.py
+++ jadnvalidation/tests/test_map.py
@@ -1,5 +1,6 @@
 
 from jadnvalidation.tests.test_utils import validate_invalid_data, validate_valid_data
+from jadnvalidation.utils.consts import XML
 
 
 def test_map():
@@ -48,6 +49,65 @@ def test_map():
             
     err_count = validate_valid_data(j_schema, root, invalid_data_list)
     assert err_count == len(invalid_data_list) 
+    
+def test_xml_map():
+    root = "Root-Test"
+    
+    j_schema = {
+        "info": {
+            "package": "http://test/v1.0",
+            "exports": ["Root-Test"]
+        },
+        "types": [
+            ["Root-Test", "Map", [], "", [
+                [1, "field_value_1", "String", [], ""],
+                [2, "field_value_2", "String", [], ""]
+            ]]
+        ]
+    }
+    
+    valid_xml_1 = """<Root-Test>
+        <field_value_1>test field</field_value_1>
+        <field_value_2>Darth Andeddu</field_value_2>
+    </Root-Test>"""
+    valid_xml_2 = """<Root-Test>
+        <field_value_1>testing more</field_value_1>
+        <field_value_2>Darth Atrius</field_value_2>
+    </Root-Test>"""
+    valid_xml_3 = """<Root-Test>
+        <field_value_1>testing more 123</field_value_1>
+        <field_value_2>Darth Bane</field_value_2>
+    </Root-Test>"""
+    
+    
+    invalid_xml_1 = """<Root-Test>
+        <field_value_1b>test field</field_value_1b>
+        <field_value_2b>Darth Andeddu</field_value_2b>
+    </Root-Test>"""
+    invalid_xml_2 = """<Root-Test>
+        <field_value_1>testing more</field_value_1>
+    </Root-Test>"""
+    invalid_xml_3 = """<Root-Test>
+        <field_value_2>Darth Bane</field_value_2>
+    </Root-Test>"""       
+    
+    valid_data_list = [
+        valid_xml_1,
+        valid_xml_2,
+        valid_xml_3       
+    ]
+    
+    invalid_data_list = [
+        invalid_xml_1,
+        invalid_xml_2,
+        invalid_xml_3        
+    ]
+    
+    err_count = validate_valid_data(j_schema, root, valid_data_list, XML)    
+    assert err_count == 0
+            
+    err_count = validate_valid_data(j_schema, root, invalid_data_list, XML)
+    assert err_count == len(invalid_data_list)     
     
 def test_map_min_max():
     root = "Root-Test"
