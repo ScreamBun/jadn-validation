8873aa091c8c965b2cdfb153755d779c72338798
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-04-16 14:51:50 -0400

    unit test for xml choice working

--- jadnvalidation/tests/test_choice.py
+++ jadnvalidation/tests/test_choice.py
@@ -133,6 +133,49 @@ def test_choice_id():
     err_count = validate_valid_data(j_schema, root, invalid_data_list)
     assert err_count == len(invalid_data_list)
     
+def test_xml_choice_id():
+    root = "Root-Test"
+        
+    j_schema = {
+        "info": {
+            "package": "http://test.com",
+            "exports": ["Root-Test"]
+        },
+        "types": [
+            ["Root-Test", "Choice", ["="], "", [
+                [1, "field_value_1", "String", [], ""],
+                [2, "field_value_2", "Boolean", [], ""]
+            ]]
+        ]
+    }
+    
+    valid_xml_1 = """<Root-Test>
+        <field_value_1 key="1">illum repellendus nobis</field_value_1>
+    </Root-Test>"""
+    
+    valid_xml_2 = """<Root-Test>
+        <field_value_2 id="2">False</field_value_2>
+    </Root-Test>"""
+    
+    invalid_xml_1 = """<Root-Test>
+        <field_value_1 id="1">illum repellendus nobis</field_value_1>
+        <field_value_2 id="2">True</field_value_2>
+        <field_value_3 id="3">test extra field validation</field_value_3>
+    </Root-Test>"""
+    
+    invalid_xml_2 = """<Root-Test>
+        <field_value_x key="11">test incorrect field name</field_value_x>
+    </Root-Test>"""
+
+    valid_data_list = [valid_xml_1, valid_xml_2]
+    invalid_data_list = [invalid_xml_1, invalid_xml_2]    
+    
+    err_count = validate_valid_data(j_schema, root, valid_data_list, XML)    
+    assert err_count == 0
+            
+    err_count = validate_invalid_data(j_schema, root, invalid_data_list, XML)
+    assert err_count == len(invalid_data_list)  
+    
     
 def test_choice_anyOf():
     root = "Root-Test"
