038e84a0dd5da3e387c4378f03505babf35a767b
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-04-21 13:05:36 -0400

    removed testing assert from app logic

--- jadnvalidation/data_validation/formats/uri_template.py
+++ jadnvalidation/data_validation/formats/uri_template.py
@@ -12,11 +12,9 @@ class UriTemplate:
         Validates if a string conforms to the RFC 3387 URI template format.
         """
         if isinstance(self.data , str) and self.data.__contains__('{'):
-            try:
-                # Parse the string using RFC 3387
-                #uri = get_compiled_pattern ('^%(URI)s$')
-                #print(self.data)
-                assert uri_template.validate(self.data)
+            try:                
+                if not uri_template.validate(self.data):
+                    raise ValueError(f"Invalid URI template: {self.data}")
             except ValueError:
                 raise ValueError(f"Invalid URI template: {self.data}")
         else: 
