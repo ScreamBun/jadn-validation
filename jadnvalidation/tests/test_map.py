16675cb84944e1a0dd4644be126561070cf71ec3
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-04-23 10:26:24 -0400

    included test_xml_map_id and updated whl

--- jadnvalidation/tests/test_map.py
+++ jadnvalidation/tests/test_map.py
@@ -201,7 +201,63 @@ def test_map_id():
     assert err_count == 0
             
     err_count = validate_invalid_data(j_schema, root, invalid_data_list)
+    assert err_count == len(invalid_data_list)
+    
+
+def test_xml_map_id():
+    root = "Root-Test"
+    
+    j_schema = {
+        "info": {
+            "package": "http://test/v1.0",
+            "exports": ["Root-Test"]
+        },
+        "types": [
+            ["Root-Test", "Map", ["="], "", [
+                [1, "field_value_1", "String", [], ""],
+                [2, "field_value_2", "String", [], ""]
+            ]]
+        ]
+    }
+    
+    valid_xml_1 = """<Root-Test>
+        <field_value_1 key="1">test field</field_value_1>
+        <field_value_2 key="2">Darth Andeddu</field_value_2>
+    </Root-Test>"""
+    valid_xml_2 = """<Root-Test>
+        <field_value_1 id="1">testing more</field_value_1>
+        <field_value_2 id="2">Darth Atrius</field_value_2>
+    </Root-Test>"""
+    
+    invalid_xml_1 = """<Root-Test>
+        <field_value_1>test field</field_value_1>
+        <field_value_2>Darth Andeddu</field_value_2>
+    </Root-Test>"""
+    invalid_xml_2 = """<Root-Test>
+        <field_value_1 key="1"></field_value_1>
+        <field_value_2 key="2">Darth Andeddu</field_value_2>
+    </Root-Test>"""
+    invalid_xml_3 = """<Root-Test>
+        <field_value_1 key="1">Darth Raine</field_value_1>
+    </Root-Test>"""       
+    
+    valid_data_list = [
+        valid_xml_1,
+        valid_xml_2   
+    ]
+    
+    invalid_data_list = [
+        invalid_xml_1,
+        invalid_xml_2,
+        invalid_xml_3        
+    ]
+    
+    err_count = validate_valid_data(j_schema, root, valid_data_list, XML)    
+    assert err_count == 0
+            
+    err_count = validate_valid_data(j_schema, root, invalid_data_list, XML)
     assert err_count == len(invalid_data_list) 
+    
     
 def test_map_ref_field():
     root = "Root-Test"
