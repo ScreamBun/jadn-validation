8a6ace9d2a8a5b3961e4285f1cbc3b1192547646
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-05-09 15:38:40 -0400

    added the get_format call

--- jadnvalidation/data_validation/integer.py
+++ jadnvalidation/data_validation/integer.py
@@ -2,7 +2,7 @@ from typing import Union
 from jadnvalidation.models.jadn.jadn_config import Jadn_Config, get_j_config
 from jadnvalidation.models.jadn.jadn_type import Jadn_Type, build_j_type
 from jadnvalidation.utils.consts import JSON, XML
-from jadnvalidation.utils.mapping_utils import get_max_length, get_min_length, get_opts
+from jadnvalidation.utils.mapping_utils import get_format, get_max_length, get_min_length, get_opts
 from jadnvalidation.utils.general_utils import split_on_first_char, create_fmt_clz_instance
 
 
@@ -75,60 +75,69 @@ class Integer:
                 return None         
         
     def check_format(self):
-        val = None
+        # val = None
         
-        opts = get_opts(self.j_type)
-        for opt in opts:
-            opt_key, opt_val = split_on_first_char(opt)
+        format = get_format(self.j_type)
+        if format is not None:
+            fmt_clz_instance = create_fmt_clz_instance(format, self.data)
+            fmt_clz_instance.validate()        
+          
+        # opts = get_opts(self.j_type)
+        # for opt in opts:
+        #     opt_key, opt_val = split_on_first_char(opt)
             
-            if "/" == opt_key:
-                if opt_val in ["date", "data-time", "gYear", "gMonthDay", "gYearMonth"]:
-                    """Set aside formats with bizzarre entry possibilities, regex checks, or odd logic that is 
-                    outside the usual size or value constraint paradigm for integer formats. 
-                    Think Dates; LogicalIntegers the user may attempt to represent with... 
-                    let us be charitable and say UNIQUE formatting applied to them"""
-                    cls = create_fmt_clz_instance(opt_val, self.data)
-                    cls.validate()
-                    break
-                else:
+        #     if "/" == opt_key:
+        #         if opt_val in ["date", "data-time", "gYear", "gMonthDay", "gYearMonth"]:
+        #             """Set aside formats with bizzarre entry possibilities, regex checks, or odd logic that is 
+        #             outside the usual size or value constraint paradigm for integer formats. 
+        #             Think Dates; LogicalIntegers the user may attempt to represent with... 
+        #             let us be charitable and say UNIQUE formatting applied to them"""
+        #             cls = create_fmt_clz_instance(opt_val, self.data)
+        #             cls.validate()
+        #             break
+        #         else:
 
-                    val = opt_val
-                    format_min: int = None
-                    format_max: int = None
-                    format_min = self.give_format_constraint(val, 0)
-                    format_max = self.give_format_constraint(val, 1)
-                    if self.data > format_max:
-                        self.errors.append(f"Data exceeds allowed format length: {format_max}")
-                    if self.data < format_min:
-                        self.errors.append(f"Data does not meet minimum format length: {format_min}")
-                val = opt_val
-                format_min = None
-                format_max = None
-                format_min = self.give_format_constraint(val, 0)
-                format_max = self.give_format_constraint(val, 1)
+        #             val = opt_val
+        #             format_min: int = None
+        #             format_max: int = None
+        #             format_min = self.give_format_constraint(val, 0)
+        #             format_max = self.give_format_constraint(val, 1)
+        #             if self.data > format_max:
+        #                 self.errors.append(f"Data exceeds allowed format length: {format_max}")
+        #             if self.data < format_min:
+        #                 self.errors.append(f"Data does not meet minimum format length: {format_min}")
+        #         val = opt_val
+        #         format_min = None
+        #         format_max = None
+        #         format_min = self.give_format_constraint(val, 0)
+        #         format_max = self.give_format_constraint(val, 1)
                 
-                if self.data > format_max:
-                    self.errors.append(f"Data for type {self.j_type} exceeds allowed format length: {format_max}")
+        #         if self.data > format_max:
+        #             self.errors.append(f"Data for type {self.j_type} exceeds allowed format length: {format_max}")
                     
-                if self.data < format_min:
-                    self.errors.append(f"Data for type {self.j_type} does not meet minimum format length: {format_min}")
+        #         if self.data < format_min:
+        #             self.errors.append(f"Data for type {self.j_type} does not meet minimum format length: {format_min}")
 
     def json_check_type(self):
         if self.data is not None:
-                  
             
+            format = get_format(self.j_type)
+                  
             # TODO: Boolean check needed, True = 1, False = 0 in python
             if not isinstance(self.data, int):
-                opts = get_opts(self.j_type)                
+                # opts = get_opts(self.j_type)                
                 if isinstance(self.data, bool):
                     raise ValueError(f"Data for type {self.j_type.type_name} must be of type integer, not Boolean. Received: {type(self.data)}")  
-                elif (any( all(v in value for v in ('/date','date-time','/gYear','gMonthDay','gYearMonth'))) for value in opts):                    
+                elif format in ['date','date-time','gYear','gMonthDay','gYearMonth']:   
                     """Specific formats may allow users to enter formatted strings in place of integers. this is their enumeration in this check."""
                     if not isinstance(self.data, str):
                         raise ValueError(f"Data for type {self.j_type.type_name} not a parseable type. Received: {type(self.data)}")
 
-                else: raise ValueError(f"Data for type {self.j_type.type_name} must be of type integer. Received: {type(self.data)}")
-                raise ValueError(f"Data for type {self.j_type.type_name} must be of type integer. Received: {type(self.data)}")        
+                else: 
+                    raise ValueError(f"Data for type {self.j_type.type_name} must be of type integer. Received: {type(self.data)}")
+                
+            else:
+                pass
             
     def xml_check_type(self):
         if self.data is not None:
