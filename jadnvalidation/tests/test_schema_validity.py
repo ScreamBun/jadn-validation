abbc4994e3db3667e8f42a386e08b92f5947bfb7
Kevin Cressman <kgcressman@praxiseng.com>
2025-08-21 16:12:52 -0400

    barebones tests that pass for schema validity; more incoming imminently!

--- jadnvalidation/tests/test_schema_validity.py
+++ jadnvalidation/tests/test_schema_validity.py
@@ -269,8 +269,55 @@ def test_total_validity():
     valid_data_list = [ 
         {
             "info" : {"package" : "http://example.fake"},
-            "types" : [["Typename", "Binary", [], "", [{"String": []}]]]    
+            "types" : [["Typename", "Binary", [], ""]]        
+        },        
+        {
+            "info" : {"package" : "http://example.fake"},
+            "types" : [["Typename", "String", ["[0"], "", []]]        
+        },             
+        {
+            "info" : {"package" : "http://example.fake"},
+            "types" : [["Typename", "Integer", ["[0"], "", []]]        
+        },          
+        {
+            "info" : {"package" : "http://example.fake"},
+            "types" : [["Typename", "Number", ["[0"], "", []]]        
+        },    
+        {
+            "info" : {"package" : "http://example.fake"},
+            "types" : [["Typename", "Array", [], "", [
+                [1, "thing", "String", [], ""]]
+
+            ]]        
+        },      
+        {
+            "info" : {"package" : "http://example.fake"},
+            "types" : [["Typename", "Record", [], "", [
+                [1, "thing", "String", [], ""]]
+            ]]        
+        },      
+        {
+            "info" : {"package" : "http://example.fake"},
+            "types" : [["Typename", "Map", [], "", [
+                [1, "thing", "String", [], ""]]
+
+            ]]        
+        },      
+        {
+            "info" : {"package" : "http://example.fake"},
+            "types" : [["Typename", "ArrayOf", ["*String"], ""
+
+            ]]        
+        },      
+        {
+            "info" : {"package" : "http://example.fake"},
+            "types" : [["Typename", "MapOf", ["+Integer", "*String"], ""
+
+            ]]        
         }
+
+        
+      
     ]
     invalid_data_list = [
 
@@ -284,169 +331,6 @@ def test_total_validity():
     err_count = validate_invalid_data(j_schema, root, invalid_data_list)
     assert err_count == len(invalid_data_list) 
 
-def test_jadn_renamed():
-    root = "Schema"
-    
-    j_schema = {
-    "info": {
-      "title": "JADN Metaschema",
-      "package": "http://oasis-open.org/openc2/jadn/v2.0/schema",
-      "description": "Syntax of a JSON Abstract Data Notation (JADN) package.",
-      "license": "CC-BY-4.0",
-      "exports": ["Schema"],
-      "config": {
-        "$FieldName": "^[$A-Za-z][_A-Za-z0-9]{0,63}$"
-      }
-    },
-        "types": [
-            
-            ["Schema", "Record", [], "", [
-                [1, "info", "Metadata", [], ""],
-                [2, "types", "Type", ["[1", "]-1"], ""]]],
-
-            ["Metadata", "Map", [], "Information about this package", [
-              [1, "package", "Namespace", [], "Unique name/version of this package"],
-              [2, "version", "String", ["{1", "[0"], "Incrementing version within package"],
-              [3, "title", "String", ["{1", "[0"], "Title"],
-              [4, "description", "String", ["{1", "[0"], "Description"],
-              [5, "comment", "String", ["{1", "[0"], "Comment"],
-              [6, "copyright", "String", ["{1", "[0"], "Copyright notice"],
-              [7, "license", "String", ["{1", "[0"], "SPDX licenseId of this package"],
-              [8, "namespaces", "PrefixNS", ["[0", "]-1"], "Referenced packages"],
-              [9, "exports", "TypeName", ["[0", "]-1"], "Roots of the type tree(s) in this package"],
-              [10, "config", "Config", ["[0"], "Configuration variables"],
-              [11, "jadn_version", "Namespace", ["[0"], "JADN Metaschema package"]
-            ]],
-
-            ["PrefixNs", "Array", [], "Prefix corresponding to a namespace IRI", [
-              [1, "prefix", "NSID", [], "Namespace prefix string"],
-              [2, "namespace", "Namespace", [], "Namespace IRI"]
-            ]],
-
-            ["Config", "Map", ["{1"], "Config vars override JADN defaults", [
-              [1, "$MaxBinary", "Integer", ["y1", "[0"], "Package max octets, default = 255"],
-              [2, "$MaxString", "Integer", ["y1", "[0"], "Package max characters, default = 255"],
-              [3, "$MaxElements", "Integer", ["y1", "[0"], "Package max items/properties, default = 255"],
-              [4, "$Sys", "String", ["{1", "}1", "[0"], "System character for TypeName, default = '.'"],
-              [5, "$TypeName", "String", ["/regex", "[0"], "Default = ^[A-Z][-.A-Za-z0-9]{0,63}$"],
-              [6, "$FieldName", "String", ["/regex", "[0"], "Default = ^[a-z][_A-Za-z0-9]{0,63}$"],
-              [7, "$NSID", "String", ["/regex", "[0"], "Default = ^([A-Za-z][A-Za-z0-9]{0,7})?$"]
-            ]],
-
-            ["Namespace", "String", ["/uri"], "Unique name of a package"],
-
-            ["NSID", "String", ["%^([A-Za-z][A-Za-z0-9]{0,7})?$"], "Namespace prefix matching $NSID"],
-
-            ["TypeName", "String", ["%^[A-Z][-.A-Za-z0-9]{0,63}$"], "Name of a logical type"],
-
-            ["FieldName", "String", ["%^[a-z][_A-Za-z0-9]{0,63}$"], "Name of a field in a structured type"],
-
-            ["TypeRef", "String", [], "Reference to a type, matching ($NSID ':')? $TypeName"],
-
-            ["Type", "Array", [], "", [
-              [1, "type_name", "TypeName", [], ""],
-              [2, "core_type", "JADN-Type-Enum", [], ""],
-              [3, "type_options", "Options", ["[0"], ""],
-              [4, "type_description", "Description", ["[0"], ""],
-              [5, "fields", "JADN-Type", ["[0", "]-1"], ""]
-            ]],
-
-            ["JADN-Type-Enum", "Enumerated", [], "", [
-              [1, "Binary", ""],
-              [2, "Boolean", ""],
-              [3, "Integer", ""],
-              [4, "Number", ""],
-              [5, "String", ""],
-              [6, "Enumerated", ""],
-              [7, "Choice", ""],
-              [8, "Array", ""],
-              [9, "ArrayOf", ""],
-              [10, "Map", ""],
-              [11, "MapOf", ""],
-              [12, "Record", ""]
-            ]],
-
-            ["JADN-Type", "Choice", [], "", [
-              [1, "Binary", "Empty", [], ""],
-              [2, "Boolean", "Empty", [], ""],
-              [3, "Integer", "Empty", [], ""],
-              [4, "Number", "Empty", [], ""],
-              [5, "String", "Empty", [], ""],
-              [6, "Enumerated", "Items", [], ""],
-              [7, "Choice", "Fields", [], ""],
-              [8, "Array", "Fields", [], ""],
-              [9, "ArrayOf", "Empty", [], ""],
-              [10, "Map", "Fields", [], ""],
-              [11, "MapOf", "Empty", [], ""],
-              [12, "Record", "Fields", [], ""]
-            ]],
-            ["Empty", "Array", ["}0"], "", []],
-
-            ["Items", "ArrayOf", ["*Item"], ""],
-
-            ["Fields", "String", ["*Field"], ""],
-
-            ["Item", "Array", [], "", [
-              [1, "item_id", "FieldID", [], ""],
-              [2, "item_value", "String", [], ""],
-              [3, "item_description", "Description", ["[0"], ""]
-            ]],
-
-            ["Field", "Array", [], "", [
-              [1, "field_id", "FieldID", [], ""],
-              [2, "field_name", "FieldName", [], ""],
-              [3, "field_type", "TypeRef", [], ""],
-              [4, "field_options", "Options", ["[0"], ""],
-              [5, "field_description", "Description", ["[0"], ""]
-            ]],
-
-            ["FieldID", "Integer", ["y0"], ""],
-
-            ["Options", "ArrayOf", ["*Option"], ""],
-
-            ["Option", "String", ["{1"], ""],
-
-            ["Description", "String", [], ""]
-            ]                  
-    }  
-    
-    valid_data_list = [
-
-        {
-            "info" : {"package" : "http://example.fake"},
-            "types" : [["Typename", "Binary", [], ""]]        
-        },        
-        {
-            "info" : {"package" : "http://example.fake"},
-            "types" : [["Typename", "String", ["[0"], "", []]]        
-        },        
-        {
-            "info" : {"package" : "http://example.fake"},
-            "types" : [["Typename", "ArrayOf", ["*Integer"], ""]]        
-        },         
-        {
-            "info" : {"package" : "http://example.fake"},
-            "types" : [["Typename", "Array", [], "", [{
-                "Array": [1, "Thing1", "String", [], ""]}]
-
-            ]]        
-        }
-
-        
-      ]
-    
-    invalid_data_list = [
-        {
-            "field_value_2": "test 2",
-            "field_value_3": "test 3",
-        }
-    ]
-        
-    err_count = validate_valid_data(j_schema, root, valid_data_list)    
-    assert err_count == 0
-            
-    err_count = validate_invalid_data(j_schema, root, invalid_data_list)
-    assert err_count == len(invalid_data_list)
 
 def test_jadn_minimalist():
     root = "Schema"
