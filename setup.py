3635976e4d557a65b1bf381d0e2ce0ca4930055d
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-09-05 10:28:39 -0400

    inlcuded and updated the j_meta_schema

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="jadnvalidation",
     version="2.0.0", 
-    packages=["jadnvalidation", "jadnvalidation.data_validation", "jadnvalidation.data_validation.formats", "jadnvalidation.models", "jadnvalidation.models.jadn", "jadnvalidation.utils", "jadnvalidation.models.jadn"],
+    packages=["jadnvalidation", "jadnvalidation.data_validation", "jadnvalidation.data_validation.formats", "jadnvalidation.data_validation.schemas", "jadnvalidation.models", "jadnvalidation.models.jadn", "jadnvalidation.utils", "jadnvalidation.models.jadn"],
     install_requires=[
         "jsonpointer",
         "pytest",
